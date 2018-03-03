bdcfafa51c7177ab9752b153b943c34ba50d6656
Katsuya Tajima <zakichan1216@gmail.com>
2018-03-03 21:30:17 +0900

    delete header

--- content_script.js
+++ content_script.js
@@ -1,7 +1,16 @@
-$(document).on("click", "a", function(e){
+$(document).on("click", "a", function(e) {
   var url = e.target.href;
-  if ( url.indexOf('issues') != -1) {
-    $.colorbox({width:"1140", height:"80%", href:url});
+  if ( url.indexOf('issues') != -1 ) {
+    $.colorbox({
+      width:"1140",
+      height:"80%",
+      href:url,
+      speed:200,
+      onComplete: function() {
+        $("#cboxLoadedContent .Header").remove();
+        $("#cboxLoadedContent .pagehead").replaceWith('<div>　</div>')
+      }
+    });
     return false;
   }
 });
