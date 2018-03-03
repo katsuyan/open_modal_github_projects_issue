3a3f590f189f6b6edd212ebaa1d10949dab232fa
Katsuya Tajima <zakichan1216@gmail.com>
2018-03-03 15:27:58 +0900

    fix

--- content_script.js
+++ content_script.js
@@ -1,8 +1,6 @@
 $(document).on("click", "a", function(e){
   var url = e.target.href;
   if ( url.indexOf('issues') != -1) {
-    event.preventDefault();
-    console.log(url)
     $.colorbox({width:"1140", height:"80%", href:url});
     return false;
   }
