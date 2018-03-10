1dfd341f11a1edede6d4a209c545be301521eae2
Katsuya Tajima <zakichan1216@gmail.com>
2018-03-10 15:23:56 +0900

    fix open*2

--- content_script.js
+++ content_script.js
@@ -2,13 +2,17 @@ $(document).on("click", "a", function(e) {
   var url = e.target.href;
   if ( url.indexOf('/issues/') != -1 || url.indexOf('/pull/') != -1 ) {
     $.colorbox({
+      href:url,
+      speed:0
+    });
+    $.colorbox({
       width:"1140",
-      height:"80%",
+      height:"85%",
       href:url,
       speed:200,
       onComplete: function() {
         $("#cboxLoadedContent .Header").remove();
-        $("#cboxLoadedContent .pagehead").replaceWith('<div>　</div>')
+        $("#cboxLoadedContent .pagehead").replaceWith('<div>　</div>');
       }
     });
     return false;
