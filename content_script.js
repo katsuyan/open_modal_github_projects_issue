d004b278ec71e587b3facb8828fef78d7900a5b4
Katsuya Tajima <zakichan1216@gmail.com>
2018-03-11 21:56:14 +0900

    function addition

--- content_script.js
+++ content_script.js
@@ -1,7 +1,9 @@
-$(document).on("click", "a", function(e) {
+$(document).on("click", ".issue-card a", function(e) {
   var url = e.target.href;
   if ( url.indexOf('/issues/') != -1 || url.indexOf('/pull/') != -1 ) {
     $.colorbox({
+      width:"0",
+      height:"0",
       href:'https://github.com/issues',
       speed:0
     });
@@ -12,7 +14,9 @@ $(document).on("click", "a", function(e) {
       speed:200,
       onComplete: function() {
         $("#cboxLoadedContent .Header").remove();
-        $("#cboxLoadedContent .pagehead").replaceWith('<div>　</div>');
+        $("#cboxLoadedContent .pagehead").replaceWith('<a href="'+url+'" class="btn btn-light btn-sm">open new tab</a>');
+        $("#cboxLoadedContent a[href$='/issues/new']").remove();
+        $('#cboxLoadedContent a').attr('target', '_blank');
       }
     });
     return false;
